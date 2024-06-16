4715b4173c9ebe5f5a97647a65eaffde7a32f9e2
kc++ <2745296803@qq.com>
2024-06-16 16:21:37 +0800

    用户聊天

--- src/main/java/org/example/javachat/Chat.java
+++ src/main/java/org/example/javachat/Chat.java
@@ -2,6 +2,7 @@ package org.example.javachat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
@@ -29,7 +30,20 @@ public class Chat
     public void initialize()
     {
         userName.setText(username);
-        System.out.println(messages);
+        for (Map<String, Object> message : messages)
+        {
+            HBox messageLabel = new HBox();
+            HBox textLabel = new HBox();
+            messageLabel.getChildren().add(new Label(message.get("send_user_name") + "  "));
+            messageLabel.getChildren().add(new Label(message.get("date") + "  "));
+            textLabel.getChildren().add(new Label(message.get("text") + "  "));
+            /*
+            设置好看的样式
+            背景半透明，边框，圆角，字体颜色
+            */
+            messageLabel.setStyle("-fx-background-color: rgba(255, 0.5, 0.5, 0.1); -fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-text-fill: #000000;");
+            textLabel.setStyle("-fx-background-color: rgba(255, 0.5, 0.5, 0.1); -fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-text-fill: #000000; -fx-padding: 5px;");
+            chatVBox.getChildren().addAll(messageLabel , textLabel);
+        }
     }
-
 }
